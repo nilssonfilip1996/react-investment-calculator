930893f789e2884b67ec5a2ff259e7018c982f6b
nilssonfilip1996 <nilssonfilip1996@hotmail.com>
2025-05-14 15:01:02 +0200

    Cleanup and removed a console.log that was used for testing purposes.

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,4 +1,13 @@
 
+/**
+ * Checks if a number is correctly formated.
+ * If isFloat is true, then the function checks if the input is either a whole number or a decimal number.
+ * If isFloat is false, then the function only checks if the number is a whole number.
+ * 
+ * @param {number} input integer or float. 
+ * @param {boolean} isFloat true or false. 
+ * @returns 
+ */
 function validateInput(input, isFloat){
   const regex = /^\d+$/;
   const regexFloat = /^(\d+(\.\d+)?)$/;
@@ -13,7 +22,6 @@ export default function Input({ labelName, id, value, onChangeInput, isFloat}) {
     
   function handleChange(event) {
     const isValid = validateInput(event.target.value, isFloat);
-    console.log(labelName + ": " + isValid);
     if (isValid) {
       const id = event.target.id;
       const value = isFloat ? parseFloat(event.target.value) : parseInt(event.target.value);
